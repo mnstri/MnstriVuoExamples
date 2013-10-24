5221743ab983645b654f4dbd5c43be86dbb0f364
Matti Niinimäki <m@mansteri.com>
2013-10-24 18:36:22 +0300

    added fps and bpm nodes

---
+++ nodes-source/mnstri.math.bpmToSeconds/mnstri.math.bpmToSeconds.c
@@ -0,0 +1,28 @@
+/**
+ * @file
+ * mnstri.math.bpmToSeconds node implementation.
+ *
+ * @copyright Copyright © 2013 Månsteri.
+ * This code may be modified and distributed under the terms of the MIT License.
+ */
+
+#include "node.h"
+
+VuoModuleDetails({
+    "name" : "BPM to Seconds",
+    "description" : "Converts a BPM (Beats Per Minute) value to a number you can feed to the Fire Periodically node.",
+    "keywords" : [ "bpm", "metro" ],
+    "version" : "1.0.0",
+    "node": {
+        "isInterface" : false
+    }
+});
+
+void nodeEvent
+(
+        VuoInputData(VuoReal, {"default":120.0}) bpm,
+        VuoOutputData(VuoReal) seconds
+ )
+{
+    *seconds = 60/bpm;
+}
---
+++ nodes-source/mnstri.math.fpsToSeconds/mnstri.math.fpsToSeconds.c
@@ -0,0 +1,28 @@
+/**
+ * @file
+ * mnstri.math.bpmToseconds node implementation.
+ *
+ * @copyright Copyright © 2013 Månsteri.
+ * This code may be modified and distributed under the terms of the MIT License.
+ */
+
+#include "node.h"
+
+VuoModuleDetails({
+    "name" : "FPS to Seconds",
+    "description" : "Converts an FPS (Frames Per Second) value to a number you can feed to the Fire Periodically node.",
+    "keywords" : [ "fps", "metro" ],
+    "version" : "1.0.0",
+    "node": {
+        "isInterface" : false
+    }
+});
+
+void nodeEvent
+(
+        VuoInputData(VuoReal, {"default":60.0}) fps,
+        VuoOutputData(VuoReal) seconds
+ )
+{
+    *seconds = 1/fps;
+}
