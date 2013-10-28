9d1461542684cc26f6b93565e004efa288f965cd
Matti Niinimäki <m@mansteri.com>
2013-10-28 19:25:27 +0200

    Cleaning up

--- source-for-nodes/node/mnstri.glsl.vignette/mnstri.glsl.vignette.c
+++ source-for-nodes/node/mnstri.glsl.vignette/mnstri.glsl.vignette.c
@@ -53,7 +53,7 @@ struct nodeInstanceData * nodeInstanceInit(void)
     struct nodeInstanceData * instance = (struct nodeInstanceData *)malloc(sizeof(struct nodeInstanceData));
     VuoRegister(instance, free);
 
-    instance->shader = VuoShader_make("Swap Color Channels", VuoShader_getDefaultVertexShader(), fragmentShaderSource);
+    instance->shader = VuoShader_make("Mnstri Vignette", VuoShader_getDefaultVertexShader(), fragmentShaderSource);
     VuoRetain(instance->shader);
     instance->imageRenderer = VuoImageRenderer_make();
     VuoRetain(instance->imageRenderer);
@@ -65,12 +65,6 @@ void nodeInstanceEvent
 (
         VuoInstanceData(struct nodeInstanceData *) instance,
         VuoInputData(VuoImage) image,
-        //VuoInputData(VuoReal, {"default":1,"suggestedMin":0,"suggestedMax":1,"suggestedStep":0.1}) red,
-        //VuoInputData(VuoReal, {"default":1,"suggestedMin":0,"suggestedMax":1,"suggestedStep":0.1}) green,
-        //VuoInputData(VuoReal, {"default":1,"suggestedMin":0,"suggestedMax":1,"suggestedStep":0.1}) blue,
-        //VuoInputData(VuoReal, {"default":0, "suggestedStep":0.01}) time,
-        //VuoInputData(VuoPoint2d, {"default":{"x":0,"y":0}}) mouse,
-        //VuoInputData(VuoPoint2d, {"default":{"x":1024,"y":768}}) resolution,
         VuoInputData(VuoReal, {"default":0.3, "suggestedStep":0.01}) innerRadius,
         VuoInputData(VuoReal, {"default":1.0, "suggestedStep":0.01}) outerRadius,
         VuoOutputData(VuoImage) adjustedImage
@@ -84,16 +78,8 @@ void nodeInstanceEvent
     VuoShader_addTexture((*instance)->shader, image, "texture");
 
     // Feed parameters to the shader.
-    /*
-    VuoShader_setUniformFloat((*instance)->shader, "red", red);
-    VuoShader_setUniformFloat((*instance)->shader, "green", green);
-    VuoShader_setUniformFloat((*instance)->shader, "blue", blue);
-    VuoShader_setUniformFloat((*instance)->shader, "time", time);
-    VuoShader_setUniformPoint2d((*instance)->shader, "mouse", mouse);
-    */
     VuoShader_setUniformFloat((*instance)->shader, "innerRadius", innerRadius);
     VuoShader_setUniformFloat((*instance)->shader, "outerRadius", outerRadius);
-    //VuoShader_setUniformPoint2d((*instance)->shader, "resolution", radius);
 
     // Render.
     *adjustedImage = VuoImageRenderer_draw((*instance)->imageRenderer, (*instance)->shader, image->pixelsWide, image->pixelsHigh);
